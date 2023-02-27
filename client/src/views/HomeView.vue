9bfb81e23110fdf2d264c55c716a214d29febbcc
Bryan De Anda <bryandeanda2012@gmail.com>
2023-02-27 14:42:50 -0500

    cleaned up

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="titel">
+      This is our home page
+    </h1>
   </main>
 </template>
