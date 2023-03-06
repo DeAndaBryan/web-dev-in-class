aa4541b821fc9a8c5f45a86078c9285026ebee43
Bryan De Anda <bryandeanda2012@gmail.com>
2023-03-06 14:08:13 -0500

    update app and login badge

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,22 +3,16 @@ import { RouterLink, RouterView } from 'vue-router'
 import NavBar from './components/NavBar.vue';
 
 
-
-
 </script>
 
 <template>
-  <NavBar />
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+    
+    <NavBar />
 
-  <RouterView />
+    <div class="container">
+        <RouterView />
+    </div>
+    
 </template>
 
 <style scoped>
