2ffe9030070c848487a7ddca0de561383271a9ac
Bryan De Anda <bryandeanda2012@gmail.com>
2023-03-20 13:56:02 -0400

    updated to class repo

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 
+
 </script>
 
 <template>
   <main>
-    <h1 class="titel">
-      This is our home page
+    <h1 class="title">
+        This is our home page
     </h1>
   </main>
 </template>
