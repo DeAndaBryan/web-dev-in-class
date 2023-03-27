a83452ed888c3e8cf8453172e3c404308b5dce13
Bryan De Anda <bryandeanda2012@gmail.com>
2023-03-27 14:17:36 -0400

    synced in class repo to mine

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,50 +1,56 @@
 const express = require('express');
-const router = express.Router();
 const model = require('../models/products');
+const router = express.Router();
 
 router
     .get('/', (req, res) => {
         const list = model.getProducts();
-        res.send({
-            items: [
-                { id: 1,
-                    name: 'item1' 
-                },
-            ]
-        })
+        res.send(list)
     })
+
+    .get('/search/:q', (req, res) => {
+        const term = req.params.q;
+        console.log({ term });
+        const list = model.searchProducts(term);
+        res.send(list);
+    })
+
     .get('/:id', (req, res) => {
         const id = +req.params.id;
         const product = model.getProductById(id);
         res.send(product);
     })
+
     .post('/', (req, res) => {
         const product = req.body;
 
-        console.log({ product }});
-        console.log({ req.query }});
-        console.log({ req.params }});
-        console.log({ req.headers }});
+        console.log({ product });
+        console.log( req.query );
+        console.log( req.params );
+        console.log( req.headers );
 
         model.addProduct(product);
         res.send(product);
     })
+
     .patch('/:id', (req, res) => {
         const product = req.body;
         model.updateProduct(product);
         res.send(product);
     })
+
     .delete('/:id', (req, res) => {
         const id = +req.params.id;
         model.deleteProduct(id);
-        res.send({ id });
+        res.send({id});
     })
 
-    module.exports = router;
+module.exports = router;
 
-    /* Ways to pass information to the server:
-    1. Query string: http://localhost:3000/products?name=abc
-    2. Route parameters: http://localhost:3000/products/1
-    3. Request body: http://localhost:3000/products
-    4. Headers: http://localhost:3000/products
-    */
+/*  Ways to pass information to the server:
+    1. Query String Parameters
+    2. Route Parameters
+    3. Request Body
+    4. Request Headers
+    5. Cookies
+*/
