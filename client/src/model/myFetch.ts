91845111d86838195cbed2543e3f4792b965f4fa
Bryan De Anda <bryandeanda2012@gmail.com>
2023-04-20 14:30:17 -0400

    refactored

--- client/src/model/myFetch.ts
+++ client/src/model/myFetch.ts
@@ -1,6 +1,6 @@
 /*  B"H
 */
-const API_URL = import.meta.env ?? 'http://localhost:3000/api/v1/';
+const API_URL = import.meta.env ?? ' /api/v1/';
 
 export function rest(url: string, data?: any, method?: string, headers?: any){
     return fetch(url, {
